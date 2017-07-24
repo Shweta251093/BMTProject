d8ecd8ec3358d7ac7d052a1402f1c0ab36d1400d
Shweta251093 <ozashwetaa@gmail.com>
2017-07-24 12:31:07 +0530

    changed

--- src/main/java/com/nttdata/pune/training/bookmytable/util/AdminRowMapper.java
+++ src/main/java/com/nttdata/pune/training/bookmytable/util/AdminRowMapper.java
@@ -15,6 +15,7 @@ public class AdminRowMapper implements RowMapper<LoginBean> {
 	@Override
 	public LoginBean mapRow(ResultSet rs, int rowNum) throws SQLException {
 	
+	//checking
 		/*----------------------------------  Sets the designated parameter to the given Java String value---------*/
 		LoginBean adminlist = new LoginBean();
 		adminlist.setUsername(rs.getString("hotel_id"));//Retrieves the value of the designated column in the current row
